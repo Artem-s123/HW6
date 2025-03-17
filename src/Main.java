15aa1c76c5ca1184d1a2f5d4fa1e93dd272b9abd
Nitro <artemcuker@gmail.com>
2025-03-17 23:41:22 +0100

    HW6

--- src/Main.java
+++ src/Main.java
@@ -64,5 +64,85 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println("2 * " + i + " = " + (2 * i));
         }
+
+        //Новые задачи
+        // Задача 1: Накопления с фиксированной суммой
+        int totalSavings = 0;
+        int monthlyDeposit = 15000;
+        int targetAmount = 2459000;
+        int months = 0;
+        while (totalSavings < targetAmount) {
+            totalSavings += monthlyDeposit;
+            months++;
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + totalSavings + " рублей");
+        }
+
+        // Задача 2: Вывод чисел с помощью циклов
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        // Задача 3: Население через 10 лет
+        int population = 12000000;
+        int birthRate = 17;  // на 1000 человек
+        int deathRate = 8;   // на 1000 человек
+        for (int year = 1; year <= 10; year++) {
+            population += (population / 1000) * (birthRate - deathRate);
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+        }
+
+        // Задача 4: Накопления с процентами
+        double depositAmount = 15000;
+        double bankRate = 1.07; // 7% в месяц
+        int monthCount = 0;
+        while (depositAmount < 12000000) {
+            depositAmount *= bankRate;
+            monthCount++;
+            System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + (int) depositAmount);
+        }
+
+        // Задача 5: Вывод накоплений каждые 6 месяцев
+        depositAmount = 15000;
+        monthCount = 0;
+        while (depositAmount < 12000000) {
+            depositAmount *= bankRate;
+            monthCount++;
+            if (monthCount % 6 == 0) {
+                System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + (int) depositAmount);
+            }
+        }
+
+        // Задача 6: Накопления каждые полгода в течение 9 лет
+        depositAmount = 15000;
+        for (int monthsPassed = 1; monthsPassed <= 9 * 12; monthsPassed++) {
+            depositAmount *= bankRate;
+            if (monthsPassed % 6 == 0) {
+                System.out.println("Прошло " + (monthsPassed / 6) + " полгода, сумма накоплений: " + (int) depositAmount);
+            }
+        }
+
+        // Задача 7: Определение пятниц
+        int firstFriday = 3; // Например, 3-е число - первая пятница месяца
+        for (int day = firstFriday; day <= 31; day += 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+        }
+
+        // Задача 8: Пролет кометы
+        int currentYear = 2024;
+        int startYear = currentYear - 200;
+        int endYear = currentYear + 100;
+        for (int year = 0; year <= endYear; year += 79) {
+            if (year >= startYear) {
+                System.out.println(year);
+            }
+        }
     }
 }
